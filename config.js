1a3eafae2d859e68a888fcffacac298378a261d2
hirushan2006 <79014921+hirushan2006@users.noreply.github.com>
2024-12-21 15:15:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "jyYwQRyA#ob-MpiOX0iuIZgoMIzUBXPVpfX5dXsGs5Dnmfeq3FbU", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "Orx00QzS#dCnvfGn7xF5hr2rwCjpahLctQQgpGOk2j4c2KBaRs2Q", // Add your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya", // Add your mongodb url
 };
